4356bfde78f349108bb0a585b13ddbc5fe78c471
Tang Peng <lucky_tangp@163.com>
2021-02-17 10:27:28 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from "@chakra-ui/react"
+// import theme from "../chakra"
+import theme from '@chakra-ui/theme'
+
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <CSSReset />
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,141 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { 
+  Box,
+  Img,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Text,
+  Button,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogContent,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react'
+import { FaSistrix, FaMicrophone } from "react-icons/fa";
+import React ,{ useState,useRef } from 'react'
+import styled from "@emotion/styled"
+
+
+const RoundIcon = styled.div({
+  borderRadius: '50%',
+  backgroundColor: '#999',
+  width: 60,
+  height: 60,
+  display: "flex",
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+const IconsWrap = styled.div({
+  display: "flex",
+  flexWrap: "wrap",
+})
+
+function TestDiv () {
+  console.log('TestDiv')
+  return (
+    <div>
+      div
+    </div>
+  )
+}
 
 export default function Home() {
+  const [iconsList, setIconsList] = useState([])
+  const [isOpen, setIsOpen] = React.useState(true)
+  const onClose = () => setIsOpen(false)
+  const cancelRef = React.useRef()
+
+  const name = useRef()
+  const address = useRef()
+  const nameHandle = () => {
+    console.dir(name.current.value)
+  }
+  const addressHandle = () => {
+    console.dir(address.current.value)
+  }
+  const commit = () => {
+    setIconsList(
+      [...iconsList, {
+        icons: name.current.value,
+        address: address.current.value
+      }]
+    )
+    console.log(iconsList)
+    setIsOpen(false)
+  }
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Box w="500px" mx="auto" mt="200px">
+      <Stack textAlign="center" spacing="30px">
+        <Box><Img src="/logo.svg" mx="auto" /></Box>
+        <Box>
+          <InputGroup>
+            <InputLeftElement ml="5px" children={<FaSistrix/>}></InputLeftElement>
+            <Input placeholder="在Google 上搜索, 或者输入一个网址" shadow="base" borderRadius="20px"></Input>
+            <InputRightElement mr="5px" children={<FaMicrophone/>}></InputRightElement>
+          </InputGroup>
+        </Box>
+        <Box>
+          <IconsWrap>
+            { iconsList.map(icon => {
+              return(
+                <Box as="a" key={icon.address} href={icon.address}>
+                  <RoundIcon>{icon.icons}</RoundIcon>
+                  <Text fontSize="12px">{icon.icons}</Text>
+                </Box>
+              )
+            })
+            }
+            <Box onClick={() => {setIsOpen(!isOpen)}}>
+              <RoundIcon>+</RoundIcon>
+              {/* <Text>123</Text> */}
+            </Box>
+          </IconsWrap>
+          {/* { dialogStatus ? <TestDiv/>: ''} */}
+        </Box>
+      </Stack>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              添加快捷方式
+            </AlertDialogHeader>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <AlertDialogBody>
+              <FormControl id="first-name">
+                <FormLabel>名称</FormLabel>
+                <Input ref={name} onChange={nameHandle} />
+              </FormControl>
+              <FormControl id="网址">
+                <FormLabel>网址</FormLabel>
+                <Input placeholder="http://www.baidu.com 需添加http://" ref={address} onChange={addressHandle} />
+              </FormControl>
+            </AlertDialogBody>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                取消
+              </Button>
+              <Button colorScheme="red" onClick={commit} ml={3}>
+                确定
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </Box>
+    
   )
 }
